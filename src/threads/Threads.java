5d340354ffe959698870bc4ea4125cd25f8b2f8d
jswrigh <jswrigh@localhost.localdomain>
2019-01-29 13:59:19 -0500

    Added examples somewhat graceful exit.

--- src/threads/Threads.java
+++ src/threads/Threads.java
@@ -5,18 +5,32 @@
  */
 package threads;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  * @author sterling
  */
 public class Threads {
-
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         // Demo simple threads with start and run
-        simpleThreads();
+        System.out.println("Showing simple thread execution:");
+        
+        System.out.println("Showing thread execution using runnable:");
+        runnableThreads();
+
+        System.out.println("Showing thread execution using executors:");
+        executorThreads();
+        
+        TimeUnit.SECONDS.sleep(10); // A really unsophisticated way of exiting 
+                                    // the program somewhat gracefully.
+        System.exit(0);
     }
     
     private static void simpleThreads() {
@@ -24,5 +38,18 @@ public class Threads {
         JThread thread2 = new JThread("Thread 2");
         thread1.start();
         thread2.start();
-    }    
+    }  
+    
+    private static void runnableThreads() {
+        Runnable demoTask = new RunnableExample();
+        Thread thread1 = new Thread(demoTask);
+        thread1.start();
+    }
+    
+    private static void executorThreads() {
+        ExecutorService demoService = Executors.newFixedThreadPool(10);
+        for (int i=0; i<20; i++){
+            demoService.submit(new RunnableExample());
+        }
+    }
 }
